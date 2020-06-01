3c6d5a643261c4bb23e930ada5c2a55c58429f81
kimhwan <khan@MacBook-Pro.local>
2020-06-01 18:56:10 +0900

    add comfirm modal

--- server/index.js
+++ server/index.js
@@ -1,9 +1,18 @@
 const express = require("express");
+const bodyParser = require("body-parser");
+const morgan = require("morgan");
 const app = express();
-const port = 3000;
+const port = 3001;
+
+app.use(morgan("dev"));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 app.use(express.static(__dirname + "/public/build"));
-app.get("/", (req, res) => res.send("Hello World!"));
+app.get("/", (req, res) => {
+  console.log(req.body);
+  res.send("Hello World!");
+});
 
 app.listen(port, () =>
   console.log(`Example app listening at http://localhost:${port}`)
