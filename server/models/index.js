d37055c15cc251ddc8e1512260ac7ac300e86653
kimhwan <khan@MacBook-Pro.local>
2020-06-10 10:30:31 +0900

    db change to aws-rds

--- server/models/index.js
+++ server/models/index.js
@@ -1,10 +1,15 @@
 const Sequelize = require("sequelize");
-
-const sequelize = new Sequelize("portfolio", "root", "1234", {
-  host: "localhost",
-  dialect: "mysql",
-});
-
+require("dotenv").config();
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  {
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    dialect: "mysql",
+  }
+);
 const db = {};
 
 sequelize
