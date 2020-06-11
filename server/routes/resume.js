b5f39ab81a8ca127194cf223031a12820566c396
kimhwan <khan@MacBook-Pro.local>
2020-06-11 18:11:17 +0900

    change to mailgun from sendgrid

--- server/routes/resume.js
+++ server/routes/resume.js
@@ -2,12 +2,16 @@ require("dotenv").config();
 const express = require("express");
 const words = require("../utils/randomWords");
 const fs = require("fs");
+const path = require("path");
 const mailgun = require("mailgun-js");
 const { Email } = require("../models");
 const { createRandomWord } = require("../utils/util");
 
-const router = express.Router();
+const filename = "FrontEnd_kimHwan.pdf";
+const filepath = path.join(__dirname, "/../rezume", filename);
+console.log(filepath);
 
+const router = express.Router();
 const DOMAIN = "www.artdev.me";
 const mg = mailgun({ apiKey: process.env.MAILGUN_API_KEY, domain: DOMAIN });
 
@@ -22,23 +26,22 @@ router.post("/", async (req, res) => {
       email,
     },
   });
-
   if (data === null) {
     // 인증문자 전송
-    const data = {
+    const msg = {
       from: "icebear0629@gmail.com",
       to: email,
       subject: "[인증]Rezume_김환",
       text: "email 인증을 위한 단어를 입력해주세요.",
       html: `<p>email 인증을 위한 단어를 입력해주세요.<p><strong>${word}</strong>`,
     };
+    mg.messages().send(msg, function (error, body) {
+      if (error) {
+        console.log(error);
+      }
+      console.log(body);
+    });
     try {
-      mg.messages().send(data, function (error, body) {
-        if (error) {
-          throw Error(error);
-        }
-        console.log(body);
-      });
       await Email.create({
         email,
         word,
@@ -51,9 +54,9 @@ router.post("/", async (req, res) => {
     // 이미 존재합니다.
     // 존재하면, confirm이 없으면 다시 confirm보내개 해줘야 한다.
     if (data.dataValues.confirm !== null) {
-      res.status(403).send("already exist");
+      return res.status(403).send("already exist");
     }
-    res.send("ok");
+    res.status(201).send("ok");
   }
 });
 
@@ -85,22 +88,18 @@ router.post("/comfirm", async (req, res) => {
           },
         }
       );
-      const pathToAttachment = `${__dirname}/../rezume/FrontEnd_kimHwan.pdf`;
-      const attachment = fs.readFileSync(pathToAttachment);
-      const attch = new mailgun.Attachment({
-        data: attachment,
-        filename: "FrontEnd_김환.pdf",
-      });
-      const data = {
+      const filename = "FrontEnd_kimHwan.pdf";
+      const filepath = path.join(__dirname, "/../rezume", filename);
+      const msg = {
         to: email,
         from: "icebear0629@gmail.com",
         subject: "[Rezume]김환",
         html: "<p>이력서는 한번만 제공해드립니다. 감사합니다.</p>",
-        attachments: attch,
+        attachment: filepath,
       };
-      mailgun.messages().send(data, function (error, body) {
+      mg.messages().send(msg, function (error, body) {
         if (error) {
-          throw Error(error);
+          console.log(error);
         }
         console.log(body);
       });
